a1e49bf8430eb2d16a039e112efb2814daa9aa94
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2023-11-03 10:42:44 +0000

    conversion

---
+++ 01_basics_JS/03_converionOperators.js
@@ -0,0 +1,35 @@
+/* *******************************************       Number conversion       *********************************************** */
+
+let stName="232"
+console.log(typeof stName);
+
+let stNameToNumber=Number(stName)
+console.log(stNameToNumber);
+console.log(typeof stNameToNumber);
+//Number conversion 
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false = 0;
+
+
+/* *******************************************       Boolean conversion       *********************************************** */
+
+let isLoggedIn=" "
+console.log(typeof(Boolean(isLoggedIn)));
+console.log(Boolean(isLoggedIn));
+
+
+//boolean conversion 
+// "" => false
+// "sdjsd"/"  " => true
+// 1 => true; 0 => false
+
+/* *******************************************       String conversion       *********************************************** */
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/* *******************************************       Operators       *********************************************** */
