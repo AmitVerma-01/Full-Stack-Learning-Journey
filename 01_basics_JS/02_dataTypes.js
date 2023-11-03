783ad48838d072287dd680a6e90528800030c52a
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2023-11-03 10:27:19 +0000

    datatypes

--- 01_basics_JS/02_dataTypes.js
+++ 01_basics_JS/02_dataTypes.js
@@ -1,28 +1,26 @@
 "use strict"; // treat to use newer version of javascript
-let id=23923
-// console.log(typeof id);
 
+
+let id=23923
+console.log(typeof id);
 // range of number => 2 to power 53 
 // bigint => for bigger number if need
 
-let sName="Amit Verma" 
-// console.log(typeof sName);
 
+let sName="Amit Verma" 
+console.log(typeof sName);
 //  string => ""
 
 let isLoggedIn = true;
-// console.log(typeof isLoggedIn);
-
+console.log(typeof isLoggedIn);
 //Boolean => true/false
 
 let udf;
-// console.log(typeof ds);
-
+console.log(typeof ds);
 // undefined =>  when value is not assigned or initialized
 
 let nll=null; 
-// console.log(typeof nll);
-
+console.log(typeof nll);
 // null => standalone value , also object
 
 console.table( [typeof id ,typeof nll, typeof udf]);
