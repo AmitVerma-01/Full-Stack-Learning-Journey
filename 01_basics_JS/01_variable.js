71ec527a67449f37de4996b8f8c08e695c3a670d
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2023-11-03 09:55:46 +0000

    basics

---
+++ 01_basics_JS/01_variable.js
@@ -0,0 +1,18 @@
+const studentId =1210432071; // can't be change 
+let studentName = "Amit verma";
+var city= "Lucknow";
+
+// don't use var because of block scope problem
+
+village="Barwa raja pakar";
+
+console.log(studentId);
+
+console.table([studentId , studentName , city , village]);
+
+// studentId=98293;
+studentName="Annu"
+city="kushinagar"
+village="matiyari"
+
+console.table([studentId , studentName , city , village]);
