3018e8d078dca0699f3c43d1dccd00efdf0c80c3
Amit Verma <amitvermaup57@gmail.com>
2023-11-11 15:21:57 +0530

    scope test

---
+++ 03_basics_JS/02_scope.js
@@ -0,0 +1,15 @@
+let a= 10;
+// const b= 20;
+// var c= 30;
+if(true){
+    let a= 10.3;
+    const b= 20;
+    // var c= 30;
+    // d=40
+    console.log("inner" , a);
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
